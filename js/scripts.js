ba6e80502613579cd3fc727607176c398e002ce3
edd-reyes <pedroeddie.reyes@gmail.com>
2022-10-01 14:54:08 +0800

    added another project

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,24 @@ myName.addEventListener("click", () => {
       myName.innerText = "Eddie";
   }
 });
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_l923q4o';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Your Message has been sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
